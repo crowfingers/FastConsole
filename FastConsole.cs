194dafcec33fed3bcd916500018a2b8873805d8e
crowfingers <joefatula@joefatula.com>
2022-12-30 14:23:52 -0800

    Add project files.

---
+++ FastConsole.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System.Runtime.InteropServices;
+
+class FastConsole {
+    // Interaction with Windows API to write to the console
+    [StructLayout(LayoutKind.Sequential)]
+    struct Coord {
+        public short x, y;
+        public Coord(short x, short y) {
+            this.x = x; this.y = y;
+        }
+    };
+
+    [StructLayout(LayoutKind.Explicit)]
+    struct CharInfo {
+        [FieldOffset(0)] public ushort Char;
+        [FieldOffset(2)] public short Attributes;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct Rectangle {
+        public short left, top, right, bottom;
+        public Rectangle(short left, short top, short right, short bottom) {
+            this.left = left; this.top = top; this.right = right; this.bottom = bottom;
+        }
+    }
+
+    static SafeFileHandle outputHandle = new();
+
+    // Output buffer
+    static CharInfo[] buffer = Array.Empty<CharInfo>();
+    static short width, height, left, top;
+    static short right => (short)(left + width - 1);
+    static short bottom => (short)(top + height - 1);
+    public static int WindowWidth => width;
+    public static int WindowHeight => height;
+
+    // Initialization
+    [STAThread]
+    public static void Initialize(string title, System.ConsoleColor foreground = System.ConsoleColor.Gray, ConsoleColor background = ConsoleColor.Black) {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.Title = title;
+        Maximize();
+        Console.CursorVisible = false;
+
+        GetOutputHandle();
+
+        width = (short)(Console.WindowWidth);
+        height = (short)(Console.WindowHeight);
+        left = 0;
+        top = 0;
+
+        buffer = new CharInfo[width * height];
+
+        ForegroundColor = foreground;
+        BackgroundColor = background;
+        FillBuffer(' ', ForegroundColor, BackgroundColor);
+    }
+    static void Maximize() {
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(System.IntPtr hWnd, int cmdShow);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        static extern IntPtr GetConsoleWindow();
+
+        IntPtr window = GetConsoleWindow();
+        ShowWindow(window, 3); // maximize
+
+        if (OperatingSystem.IsWindows())
+            Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+    }
+    static void GetOutputHandle() {
+        [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern SafeFileHandle CreateFile(
+            string fileName,
+            [MarshalAs(UnmanagedType.U4)] uint fileAccess,
+            [MarshalAs(UnmanagedType.U4)] uint fileShare,
+            IntPtr securityAttributes,
+            [MarshalAs(UnmanagedType.U4)] FileMode creationDisposition,
+            [MarshalAs(UnmanagedType.U4)] int flags,
+            IntPtr template);
+
+        outputHandle = CreateFile("CONOUT$", 0x40000000, 2, IntPtr.Zero, FileMode.Open, 0, IntPtr.Zero);
+        if (outputHandle.IsInvalid) throw new Exception("outputHandle is invalid!");
+    }
+
+    // Character attribute utilities
+    static short Colorset(ConsoleColor foreground, ConsoleColor background)
+        => (short)(foreground + ((short)background << 4));
+    const short overlineBit = 0x0400;
+    static short Gridset(bool overline, bool leftline, bool rightline) {
+        const short leftlineBit = 0x0800;
+        const short rightlineBit = 0x1000;
+
+        return (short)(
+            (overline ? overlineBit : 0) + (leftline ? leftlineBit : 0) + (rightline ? rightlineBit : 0)
+        );
+    }
+
+    // Public output interface
+    static ConsoleColor _ForegroundColor, _BackgroundColor;
+    public static ConsoleColor ForegroundColor {
+        get => _ForegroundColor;
+        set {
+            _ForegroundColor = value;
+            Console.ForegroundColor = (System.ConsoleColor)value;
+        }
+    }
+    public static ConsoleColor BackgroundColor {
+        get => _BackgroundColor;
+        set {
+            _BackgroundColor = value;
+            Console.BackgroundColor = (System.ConsoleColor)value;
+        }
+    }
+
+    public static void SetChar((int x, int y) coords, char c, ConsoleColor foreground, ConsoleColor background, bool overline = false, bool leftline = false, bool rightline = false, bool underline = false) {
+        SetChar((short)coords.x, (short)coords.y, c, foreground, background, overline, leftline, rightline, underline);
+    }
+    public static void SetChar(int x, int y, char c, ConsoleColor foreground, ConsoleColor background, bool overline = false, bool leftline = false, bool rightline = false, bool underline = false) {
+        SetChar((short)x, (short)y, c, foreground, background, overline, leftline, rightline, underline);
+    }
+    public static void SetChar(short x, short y, char c, ConsoleColor foreground, ConsoleColor background, bool overline = false, bool leftline = false, bool rightline = false, bool underline = false) {
+        short address = (short)(width * y + x);
+        short colorset = Colorset(foreground, background);
+        short gridset = Gridset(overline, leftline, rightline);
+
+        if (address < 0 || address >= buffer.Length) throw new Exception("Can't write to address (" + x + "," + y + ").");
+        buffer[address].Char = c;
+        buffer[address].Attributes = (short)(colorset + gridset);
+
+        if (underline) {
+            if (y == height - 1) return;
+            address = (short)(width * (y + 1) + x);
+            buffer[address].Attributes |= overlineBit;
+        }
+    }
+
+    static void FillBuffer(char c, ConsoleColor foreground, ConsoleColor background) {
+        for (int i = 0; i < buffer.Length; i++) {
+            buffer[i].Attributes = Colorset(foreground, background);
+            buffer[i].Char = c;
+        }
+    }
+
+    // Drawing the buffer to the screen
+    public static void DrawBuffer() {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteConsoleOutputW(
+          SafeFileHandle hConsoleOutput,
+          CharInfo[] lpBuffer,
+          Coord dwBufferSize,
+          Coord dwBufferCoord,
+          ref Rectangle lpWriteRegion);
+
+        Rectangle rect = new(left, top, right, bottom);
+        WriteConsoleOutputW(outputHandle, buffer, new Coord(width, height), new Coord(0, 0), ref rect);
+    }
+
+    // Reading from the buffer
+    public static (char character, ConsoleColor foreground, ConsoleColor background) ReadChar((int x, int y) coords) {
+        short address = (short)(width * coords.y + coords.x);
+        char character = (char)buffer[address].Char;
+        short attributes = buffer[address].Attributes;
+        attributes &= 0x0FF;
+        ConsoleColor foreground = (ConsoleColor)(attributes & 0x0F);
+        ConsoleColor background = (ConsoleColor)(attributes >> 4);
+        return (character, foreground, background);
+    }
+}
